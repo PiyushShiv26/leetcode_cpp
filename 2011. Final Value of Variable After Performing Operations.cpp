98a76e4d936b4b702329789869aeefc961daa358
PiyushShiv26 <piyushshiv26@gmail.com>
2026-03-07 23:45:42 +0530

    Final Value of Variable After Performing Operations

--- 2011. Final Value of Variable After Performing Operations.cpp
+++ 2011. Final Value of Variable After Performing Operations.cpp
@@ -55,8 +55,8 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int x=0;
-        for(auto& op: operations)
-            x+=(op[1]=='+')?1:-1;
+        for (auto& op: operations)
+            x+=(op[1]=='+') ? 1 : -1;
         return x;
     }
 };
