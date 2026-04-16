28f03e8b796ca8d8f85e0c542f770e8bb5687cd5
PiyushShiv26 <piyushshiv26@gmail.com>
2026-04-16 23:52:38 +0530

    Longest Repeating Character Replacement

---
+++ 424. Longest Repeating Character Replacement.cpp
@@ -0,0 +1,54 @@
+/*
+424. Longest Repeating Character Replacement
+Medium
+
+You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+There may exists other ways to achieve this answer too.
+
+Constraints:
+
+1 <= s.length <= 10^5
+s consists of only uppercase English letters.
+0 <= k <= s.length
+*/
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> freqs;
+        int res = 0, i = 0, maxFreq = 0;
+
+        for (int j = 0; j < s.size(); j++) {
+            freqs[s[j]]++;
+            maxFreq = max(maxFreq, freqs[s[j]]);
+
+            while ((j - i + 1) - maxFreq > k) {
+                freqs[s[i]]--;
+                i++;
+            }
+
+            res = max(res, j - i + 1);
+        }
+
+        return res;
+    }
+};
