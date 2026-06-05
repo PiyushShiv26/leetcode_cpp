834c7577380fc2c34a3272c5715baf7b829b4b3c
PiyushShiv26 <piyushshiv26@gmail.com>
2026-06-06 00:05:45 +0530

    Minimum Cost of Buying Candies With Discount

--- 2144. Minimum Cost of Buying Candies With Discount.cpp
+++ 2144. Minimum Cost of Buying Candies With Discount.cpp
@@ -50,11 +50,11 @@ class Solution {
 public:
     int minimumCost(vector<int>& cost) {
         sort(cost.begin(), cost.end());
-        int total =0;
+        int total = 0;
 
-        for(int i = cost.size() - 1; i>=0; i -= 3){
+        for(int i = cost.size() - 1; i >= 0; i -= 3){
             total += cost[i];
-            if(i - 1 >= 0) total += cost[i-1];
+            if(i - 1 >= 0) total += cost[i - 1];
         }
 
         return total;
