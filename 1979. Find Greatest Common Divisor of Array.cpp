ce5ecbc4c2e388c8ceec29da77b62397d4e3e7fe
PiyushShiv26 <piyushshiv26@gmail.com>
2026-07-24 00:13:29 +0530

    Find Greatest Common Divisor of Array

--- 1979. Find Greatest Common Divisor of Array.cpp
+++ 1979. Find Greatest Common Divisor of Array.cpp
@@ -45,6 +45,14 @@ using namespace std;
 
 class Solution {
 public:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
     int findGCD(vector<int>& nums) {
         int mx = *max_element(nums.begin(), nums.end()), mn = *min_element(nums.begin(), nums.end());
         return gcd(mn, mx);
